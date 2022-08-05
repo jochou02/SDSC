e732acf5a750a4be7c3fe93c36fadf9b61890c8e
touhouEUROBEAT <91570907+touhouEUROBEAT@users.noreply.github.com>
2022-08-05 13:06:51 -0700

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'conn_prototype',
+        'NAME': 'merge_test',
         'USER': 'kfrd22',
         'PASSWORD': 'sungod123',
         'HOST': '132.249.242.127',
