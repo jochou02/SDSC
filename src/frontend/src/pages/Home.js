79cfa1f106cf167c2d237d70d51dc614703b60b3
ctanma <103155826+ctanma@users.noreply.github.com>
2022-08-18 11:52:28 -0700

    home form local computer

--- src/frontend/src/pages/Home.js
+++ src/frontend/src/pages/Home.js
@@ -1,75 +1,82 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react'
+import '../../App.css'
+import "../Home.css";
 
-import LoggedInTester from '../buttons/LoggedInTester';
+export default function Home() {
+  const [state,setState] = useState(false);
 
-import GenerateMatching from './components/GenerateMatching';
-import MatchingReceived from './components/MatchingReceived';
-import Test from './components/Test';
-
-/* import Profile from './Profile' */
-
-/*
-    GenerateMatching takes care of generating a matching, as well as displaying
-    matching that we have sent.
-
-    Matching Received takes care of interaction between we and matching that we
-    have received.
-
-
-*/
-class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { foo: [], };
-
-        this.ShowProfile = this.ShowProfile.bind(this);
-    }
-
-    componentDidMount() {
-        const headers = {"Content-Type": "application/json"};
-
-        if (localStorage.getItem('auth-token')) {
-            headers["Authorization"] = localStorage.getItem('auth-token');
-        }
-
-        fetch('http://127.0.0.1:8000/connect/get_info/', { headers, })
-                    .then(response => response.json())
-                    .then((data) => {
-                    this.setState({ foo: data })
-        })
-        .catch(console.log)
-    }
-
-    ShowProfile = ({ foo }) => {
-        return (
-            <>
-                {   !localStorage.getItem('auth-token') ? <></> : <>
-                    ID: {foo['id']} <br />
-                    Name: {foo['first_name']} {foo['last_name']} <br />
-                    E-Mail: {foo['email']} <br />
-                    College: {foo['user_college']}
-                    </>
-                }
-            </>
-        );
-    }
-
-    render() {
-      return (
-        <>
-            <LoggedInTester />
-
-            {/* Example of showing user's information */}
-            <this.ShowProfile foo={this.state.foo} />
-
-            <GenerateMatching /> 
-            <MatchingReceived />
-
-            <Test />
-
-        </>
-      );
-    }
+  const toggle=()=>{
+    setState(!state);
+  }
+  return (
+      
+        <div className ='homepage-container'>
+        <div className = 'welcome-sign'>
+          <h1>Welcome, User!</h1>
+        </div>
+        <div className = 'generalConnections'>
+          <div className = 'myconnections'>
+            <div className = 'cntTitle'><h1>My Connections</h1></div>
+            <div className = 'mycntTop'>Connection Name 1</div>
+            <div className = 'mycnt'>Connection Name 2</div>
+            <div className = 'mycnt'>Connection Name 3</div>
+            <div className = 'mycnt'>Connection Name 4</div>
+          </div>
+          <div className = 'new-connections'>
+          <div className = 'reqTitle'>
+            <h1>Connection Requests</h1>
+            <button onClick = {toggle} className ={'toggle--button ' + (state ? 'toggle--close':'')}>
+              {state ? 'Opt in!' : 'Opt Out!'}
+            </button>
+          </div>
+            <div className = 'cntReq'>
+                <div className = 'reqCnt'>
+                  <div className = 'name'>Connection Request Name 1</div>
+                  <div className = 'courses'>
+                    Mutual Courses: Ex 1, Ex 2, Ex 3                     
+                    <button className= 'accept'>O</button>
+                    <button className= 'deny'>X</button>
+                  </div>
+                  <div className = 'friendRequest'>Incoming Friend Request</div>
+                </div>
+                <div className = 'reqCnt'>
+                  <div>
+                  <div className = 'name'>Connection Request Name 2</div>
+                  <div className = 'courses'>
+                    Mutual Courses: Ex 1, Ex 2, Ex 3                     
+                    <button className= 'accept'>O</button>
+                    <button className= 'deny'>X</button>
+                  </div>
+                  <div className = 'friendRequest'>Outgoing Friend Request</div>
+                  </div>
+                  <div className= 'acceptdeclinebtns'>
+                  {/* <button className= 'accept'>O</button>
+                  <button className= 'deny'>X</button> */}
+                  </div>                  
+                </div>           
+            </div>
+            <h1 className = 'recTitle'>Recommended Connections</h1>
+            <div className = 'recCnt'>
+              <div className= "con">
+                <div className = 'name'>Recommended Connection Name 1</div>
+                <div className = 'courses'>
+                    Mutual Courses: Ex 1, Ex 2, Ex 3                     
+                    <button className= 'accept'>O</button>
+                    <button className= 'deny'>X</button>
+                  </div>
+              </div>
+              <div className= "con">
+                <div className= 'name'>Recommended Connection Name 2</div>
+                <div className = 'courses'>
+                    Mutual Courses: Ex 1, Ex 2, Ex 3                     
+                    <button className= 'accept'>O</button>
+                    <button className= 'deny'>X</button>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    );
   }
 
-  export default Home
