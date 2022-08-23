89de528dedd13e55ea5b8364b8cbdd545c941022
Caleb Li <1caleblili@gmail.com>
2022-08-23 01:36:51 -0700

    created api for wait time

--- src/backend/wait_time/views.py
+++ src/backend/wait_time/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.views import APIView
 from django.views import View
 from rest_framework.response import Response
 import redis
@@ -9,22 +10,26 @@ import pprint
 def index(request):
     return HttpResponse("Hello, world. You're at the wait time index.")
 
-class wait_time_data(View):
+class wait_time_data(APIView):
     def get(self, request):
+        waitTimeList = []
         r = redis.StrictRedis(host="132.249.242.203", port=6379, db=0, password='kungfurubberducky2022')
         for key in r.scan_iter():
             if (key != None and 'wait'.encode() in key):
-                print(key, r.get(key))
-                print("")
-        print("-=-=-=-=-=-=-")
-        a = r.get('CSE 100')
-        a = ujson.loads(a)
-        print(type(a), a)
-        print(a['AvgApproval'])
-        print("****")
+                place = ujson.loads(r.get(key))
+                waitTimeList.append(place)
+        filteredList = []
+        for location in waitTimeList:
+            filter = {
+                "place" : location['name'],
+                "capacityPercentage" : location['percentage'],
+                "subLocation" : False
+            }
+            if (location['subLocs'] != False):
+                filter['subLocation'] = location['subLocs']
+            filteredList.append(filter)
+
+        #get the subLocs for Geisel Library
         b = ujson.loads(r.get('wait_7'))
         p = pprint.PrettyPrinter(indent = 4)
-        p.pprint(b)
-        print("Location: " + b['name'])
-        print("")
-        return HttpResponse("Pulling data from redis...")
+        return Response(filteredList)
