6356c9bf683ab4b321619a413246a7779e6e995d
ctanma <103155826+ctanma@users.noreply.github.com>
2022-08-23 23:13:04 -0700

    fixed the time stuff
    note: new package (npm install --save react-datetime-picker)

--- src/frontend/src/pages/Calendar.js
+++ src/frontend/src/pages/Calendar.js
@@ -6,8 +6,10 @@ import startOfWeek from "date-fns/startOfWeek";
 import React, { useState } from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import DatePicker from "react-datepicker";
+// import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+// import TimePicker from 'react-time-picker';
+import DateTimePicker from 'react-datetime-picker';
 import { toBeRequired } from '@testing-library/jest-dom/dist/matchers';
 
 // https://www.youtube.com/watch?v=lyRP_D0qCfk&ab_channel=DarwinTech --> need to add packages from this vid
@@ -27,22 +29,22 @@ const events = [
   {
       title: "Big Meeting",
       allDay: true,
-      start: new Date(2021, 8, 3),
-      end: new Date(2021, 8, 5),
+      start: new Date(2022, 7, 3,12,10,0),
+      end: new Date(2022, 7, 5,2,0,0),
   },
   {
       title: "Vacation",
-      start: new Date(2021, 8, 12),
-      end: new Date(2021, 8, 17),
+      start: new Date(2022, 7, 17,14,0,0),
+      end: new Date(2022, 7, 17,15,0,0),
   },
   {
       title: "Conference",
-      start: new Date(2021, 8, 20),
-      end: new Date(2021, 8, 25),
+      start: new Date(2022, 7, 20,8,0,0),
+      end: new Date(2022, 7, 25,10,0,0),
   },
 ];
 export default function Calendars() {
-  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: ""});
   const [allEvents, setAllEvents] = useState(events);
 
   function handleAddEvent() {
@@ -55,8 +57,12 @@ export default function Calendars() {
     <h2>Add New Event</h2>
     <div>
     <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-    <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+    {/* <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
     <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+    <TimePicker placeholderText="Start Time" disableClock ="true" amPmAriaLabel	= "Select AM/PM" style={{marginRight:"10px" }}  onChange={(start) => setNewEvent({ ...newEvent, start })} />
+    <TimePicker placeholderText="End Time" disableClock= "true" amPmAriaLabel	= "Select AM/PM"  selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} /> */}
+    <DateTimePicker placeholderText="Start Date" style={{ marginRight: "10px" }} disableCalendar ="true" disableClock ="true" selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+    <DateTimePicker placeholderText="End Date" selected={newEvent.end} disableCalendar ="true" disableClock ="true" onChange={(end) => setNewEvent({ ...newEvent, end })} />
      <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
        Add Event
      </button>
