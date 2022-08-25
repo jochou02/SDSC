81556c31b253d93b98399e12d96c776e59e232bb
touhouEUROBEAT <jis029@ucsd.edu>
2022-08-25 10:43:47 -0700

    updated wait_time backend

--- src/backend/wait_time/views.py
+++ src/backend/wait_time/views.py
@@ -12,20 +12,27 @@ def index(request):
 
 class wait_time_data(APIView):
     def get(self, request):
-        waitTimeList = []
+        # Changing how wait_time data is stored in redis to better serve Andrew's implementation
+
+        # waitTimeList = []
+        # r = redis.StrictRedis(host="132.249.242.203", port=6379, db=0, password='kungfurubberducky2022')
+        # for key in r.scan_iter():
+        #     if (key != None and 'wait'.encode() in key):
+        #         place = ujson.loads(r.get(key))
+        #         waitTimeList.append(place)
+        # filteredList = []
+        # for location in waitTimeList:
+        #     filter = {
+        #         "place" : location['name'],
+        #         "capacityPercentage" : location['percentage'],
+        #         "subLocation" : False
+        #     }
+        #     if (location['subLocs'] != False):
+        #         filter['subLocation'] = location['subLocs']
+        #     filteredList.append(filter)
+        # return Response(filteredList)
+
         r = redis.StrictRedis(host="132.249.242.203", port=6379, db=0, password='kungfurubberducky2022')
-        for key in r.scan_iter():
-            if (key != None and 'wait'.encode() in key):
-                place = ujson.loads(r.get(key))
-                waitTimeList.append(place)
-        filteredList = []
-        for location in waitTimeList:
-            filter = {
-                "place" : location['name'],
-                "capacityPercentage" : location['percentage'],
-                "subLocation" : False
-            }
-            if (location['subLocs'] != False):
-                filter['subLocation'] = location['subLocs']
-            filteredList.append(filter)
-        return Response(filteredList)
+
+        return Response(ujson.loads(r.get('wait_time')))
+
