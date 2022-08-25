16940f32a6d14d3e08dcce00a10a84916a2b50ad
touhouEUROBEAT <jis029@ucsd.edu>
2022-08-24 19:51:34 -0700

    New API for getting course data

--- src/backend/review/urls.py
+++ src/backend/review/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     
     path('<str:pk1>/<str:pk2>/', views.GetReviews.as_view(), name='get_reviews'),
-
+    path('get_course_data', views.GetCourseData.as_view(), name='get_course_data'),
+    path('get_course_data_test', views.GetCourseDataTest.as_view(), name='get_course_data_test'),
 ]
--- src/backend/review/views.py
+++ src/backend/review/views.py
@@ -6,6 +6,9 @@ from account.models import *
 from .models import *
 from api.serializers import *
 
+import redis
+import ujson
+
 # Create your views here.
 
 class GetReviews(APIView):
@@ -18,3 +21,26 @@ class GetReviews(APIView):
             data = serializer.data
             return Response(data)
 
+
+
+class GetCourseData(APIView):
+
+    def post(self, request):
+        request_content = ujson.loads(request.body.decode("utf-8"))
+        course_name = request_content['course_name']
+
+        r = redis.StrictRedis(host="132.249.242.203", port=6379, db=0, password='kungfurubberducky2022')
+
+        # print(r.get('CSE 100'))
+
+        return Response(ujson.loads(r.get(course_name)))
+
+
+class GetCourseDataTest(APIView):
+
+    def post(self, request):
+        r = redis.StrictRedis(host="132.249.242.203", port=6379, db=0, password='kungfurubberducky2022')
+
+        # print(r.get('CSE 100'))
+
+        return Response(ujson.loads(r.get('CSE 100')))
