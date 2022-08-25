3a571edbc37c233b21e4002e8d4e250556c19ef0
ctanma <103155826+ctanma@users.noreply.github.com>
2022-08-24 21:56:15 -0700

    Add files via upload

--- src/frontend/src/pages/Reviews.js
+++ src/frontend/src/pages/Reviews.js
@@ -1,19 +1,68 @@
 import React, {useState, useEffect} from 'react'
 import '../../App.css'
 import "../Reviews.css";
+import StarRatingComponent from 'react-star-rating-component';
 
 
 export default function Reviews(){
-    const[classData,setClassData] = useState([ //temporary dummy data
-        {course_name:"CSE 100", avg_approval:"93.2%", avg_hours: "11.33", avg_grade:"B (3.07)", },
-    ]);
     
-    const[review, setReviews] = useState([
-        {prof_name:"Professor A", class_time_held:"Fall 2021", rating:"5",class_review:"Oh he decisively impression attachment friendship so if everything. Whose her enjoy chief new young. Felicity if ye required likewise so doubtful. On so attention necessary at by provision otherwise existence direction. Unpleasing up announcing unpleasant themselves oh do on. Way advantage age led listening belonging supposing."},
-        {prof_name:"Professor B", class_time_held:"Spring 2020", rating:"3",class_review:"Oh he decisively impression attachment friendship so if everything. Whose her enjoy chief new young. Felicity if ye required likewise so doubtful. On so attention necessary at by provision otherwise existence direction. Unpleasing up announcing unpleasant themselves oh do on. Way advantage age led listening belonging supposing."},
-        {prof_name:"Professor C", class_time_held:"Winter 2017", rating:"4", class_review:"Oh he decisively impression attachment friendship so if everything. Whose her enjoy chief new young. Felicity if ye required likewise so doubtful. On so attention necessary at by provision otherwise existence direction. Unpleasing up announcing unpleasant themselves oh do on. Way advantage age led listening belonging supposing."},
-        {prof_name:"Professor D", class_time_held:"Summer 2019", rating:"2",class_review:"Oh he decisively impression attachment friendship so if everything. Whose her enjoy chief new young. Felicity if ye required likewise so doubtful. On so attention necessary at by provision otherwise existence direction. Unpleasing up announcing unpleasant themselves oh do on. Way advantage age led listening belonging supposing."}
+    const[classData,setClassData] = useState([ //temporary dummy data
+        {course_name:"CSE 100", avg_approval:"93.2%", avg_hours: "11.33", avg_grade:"B (3.07)", }, //not sure if names are correct
+    ]);
+    const[review, setReviews] = useState([ // rating, description, course(course_dept, course_num,prof), student
+        {prof:"Professor A", rating:"5",description:"Oh he decisively impression attachment friendship so if everything. Whose her enjoy chief new young. Felicity if ye required likewise so doubtful. On so attention necessary at by provision otherwise existence direction. Unpleasing up announcing unpleasant themselves oh do on. Way advantage age led listening belonging supposing."},
+        {prof:"Professor B", rating:"3",description:"Oh he decisively impression attachment friendship so if everything. Whose her enjoy chief new young. Felicity if ye required likewise so doubtful. On so attention necessary at by provision otherwise existence direction. Unpleasing up announcing unpleasant themselves oh do on. Way advantage age led listening belonging supposing."},
+        {prof:"Professor C", rating:"4", description:"Oh he decisively impression attachment friendship so if everything. Whose her enjoy chief new young. Felicity if ye required likewise so doubtful. On so attention necessary at by provision otherwise existence direction. Unpleasing up announcing unpleasant themselves oh do on. Way advantage age led listening belonging supposing."},
+        {prof:"Professor D", rating:"2",description:"Oh he decisively impression attachment friendship so if everything. Whose her enjoy chief new young. Felicity if ye required likewise so doubtful. On so attention necessary at by provision otherwise existence direction. Unpleasing up announcing unpleasant themselves oh do on. Way advantage age led listening belonging supposing."}
     ])
+    const[rating,makeRating] = useState("");    //holds the user's new rating
+    const[description,makeDescription]=useState(""); //holds the user's new review
+
+    
+    const onChange = (e) => {   //holds user description of rating
+        makeDescription(e.target.description);
+    }
+
+    const setRating = (e) =>{   //holds users rating # (1 to 5)
+        makeRating(e.target);
+    }
+
+    const onSubmitEvent = (e) => {  //make it so that this function creates a new review and pushes into our reviews state
+        e.preventDefault();
+        let a = {prof:"Professor A", rating:rating, description:description}
+        review.push(a)
+        console.log(review);
+    }
+
+    useEffect(()=>{
+        const headers = {"Content-Type": "application/json"};
+       
+        if (localStorage.getItem('auth-token')) {
+            headers["Authorization"] = localStorage.getItem('auth-token');
+        }
+        //fetches data for reviews
+        fetch('http://127.0.0.1:8000/review/get_reviews/', { headers, }) 
+                    .then(response => response.json())
+                    .then((data) => {
+                    setReviews(data)
+        })
+        .catch(console.log)
+
+        //for class stats
+        const courseName = {
+            method: 'POST',
+            body: JSON.stringify({
+                course_name: 'CSE 100'
+            })
+        }
+
+        fetch('http://127.0.0.1:8000/review/get_course_data/', courseName) 
+                    .then(response => response.json())
+                    .then((data) => {
+                    setClassData(data)
+        })
+        .catch(console.log)
+    })
     return (
         <div className = "ReviewPageContainer">         
             <div className = 'ReviewStats'>
@@ -46,21 +95,27 @@ export default function Reviews(){
                 </div>
             </div>
             <div className = 'ReviewSection'>
-            <button className = "reviewButton">
-                Leave A Review!
-            </button>
+            <form onSubmit = {onSubmitEvent}>
+                <textarea placeholder="Write your own review!" input type="text" 
+                className='reviewBox' onChange={onChange} onSubmit={""} 
+                />
+                
+                <StarRatingComponent className = "stars" onStarClick={setRating}></StarRatingComponent>
+                <button type ="submit" className = "reviewButton">
+                Submit Review
+                </button>
+            </form>
             <div className = 'reviewTitle'>Reviews</div>
             {/* <button className = "sortBy">
                 Sort By
             </button> */}
             {review.map(data => {
                 return <div className='reviews'>
-                <div className = 'profName'>{data.prof_name}
-                <div className ='classTime'>{data.class_time_held}</div>
+                <div className = 'profName'>{data.prof}
                 <div className = 'rating_out_5'>{data.rating}</div>
                 </div>
                 
-                <div className = 'ratingDescrption'>{data.class_review}</div>
+                <div className = 'ratingDescrption'>{data.description}</div>
                 </div>
             })}
            
